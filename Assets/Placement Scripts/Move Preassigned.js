be3c6240a53053c11f4d17bbf1455ce5c10b4d78
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-09-09 10:43:17 -0500

    Added a cube. With a script.

--- Assets/Placement Scripts/Move Preassigned.js
+++ Assets/Placement Scripts/Move Preassigned.js
@@ -2,14 +2,13 @@
 
 function Update () {
 	if (GetComponent.<Renderer>().isVisible == false) {
+	GetComponent.<Renderer>().enabled = false;
 	gameObject.isStatic = false;
 	transform.position = Pos[Random.Range(0,Pos.Length)];
 		if (GetComponent.<Renderer>().isVisible == true) {
-		GetComponent.<Renderer>().enabled = false;
 	}
 	}
 	else {
 	gameObject.isStatic = true;
-	GetComponent.<Renderer>().enabled = true;
 	}
 }
