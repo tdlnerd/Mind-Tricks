81d83f19e89e979b671f163776af3653aa89258e
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-09-09 10:34:23 -0500

    Added test scene and simple script

---
+++ Assets/Placement Scripts/Move Preassigned.js
@@ -0,0 +1,15 @@
+﻿var Pos : Vector3[];
+
+function Update () {
+	if (GetComponent.<Renderer>().isVisible == false) {
+	gameObject.isStatic = false;
+	transform.position = Pos[Random.Range(0,Pos.Length)];
+		if (GetComponent.<Renderer>().isVisible == true) {
+		GetComponent.<Renderer>().enabled = false;
+	}
+	}
+	else {
+	gameObject.isStatic = true;
+	GetComponent.<Renderer>().enabled = true;
+	}
+}
