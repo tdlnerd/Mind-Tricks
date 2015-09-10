1250eecccb6b78f83a51d1eb2f164bd0c97f8f40
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-09-10 10:16:41 -0500

    Testing

--- Assets/Placement Scripts/Move Preassigned.js
+++ Assets/Placement Scripts/Move Preassigned.js
@@ -1,10 +1,14 @@
 ﻿var Pos : Vector3[];
 
-
-function OnBecameInvisible () {
-Move();
-}
-
-function Move() {
-transform.position = Pos[Random.Range(0,Pos.Length)];
+function Update () {
+	if (GetComponent.<Renderer>().isVisible == false) {
+	GetComponent.<Renderer>().enabled = false;
+	gameObject.isStatic = false;
+	transform.position = Pos[Random.Range(0,Pos.Length)];
+		if (GetComponent.<Renderer>().isVisible == true) {
+	}
+	}
+	else {
+	gameObject.isStatic = true;
+	}
 }
